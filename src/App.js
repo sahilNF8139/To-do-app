43764b3fd91b0cb0b8b7b11290c2bd6a813d2a54
sahilNF8139 <sahilkhalani83@gmail.com>
2024-03-29 17:21:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Toodo from './components/Toodo';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcone to my profile</h1>
+      <Toodo></Toodo>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Toodo.jsx
@@ -0,0 +1,27 @@
+    
+import './Toodo.css'
+import React from 'react';
+function Toodo(){
+    return(
+        <div>
+            <div className='main-div'>
+                <div className='child-div'>
+                    <figure>
+                        <figcaption>Add Your List Here</figcaption>
+                    </figure>
+                    <div className='additems'>
+                        <input type='text ' placeholder='Add Items...'>
+                        </input>
+                        <FontAwesomeIcon icon="fa-regular fa-plus" />
+                        <i className='fa fa-plus add-btn'>
+                        
+                        </i>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+export default Toodo
